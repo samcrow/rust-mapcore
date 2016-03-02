e2ad6e8d29fa1ae9f9ca86ac8f9221df657d2eb7
Sam Crow <samcrow@uw.edu>
2016-03-01 19:29:35 -0800

    Various changes

--- src/map.rs
+++ src/map.rs
@@ -25,13 +25,21 @@ pub struct Map {
     view_projection: ViewProjection,
     /// The layers
     layers: Vec<Box<Layer>>,
+    /// The X location of the lower left corner of the map, in pixels
+    x: i32,
+    /// The Y location of the lower left corner of the map, in pixels
+    y: i32,
+    /// The width of the map, in pixels
+    width: i32,
+    /// The height of the map, in pixels
+    height: i32,
 }
 
 impl Map {
     ///
     /// Creates a new map using a given projection
     ///
-    pub fn new<P>(projection: P) -> Map where P: 'static + Projection {
+    pub fn new<P>(projection: P, x: i32, y: i32, width: i32, height: i32) -> Map where P: 'static + Projection {
         Map {
             projection: Box::new(projection),
             view_projection: ViewProjection {
@@ -39,6 +47,10 @@ impl Map {
                 zoom: 1f64,
             },
             layers: Vec::new(),
+            x: x,
+            y: y,
+            width: width,
+            height: height,
         }
     }
 
@@ -64,12 +76,48 @@ impl Map {
     }
 
     ///
-    /// Draws this map with its upper left corner at (x, y) with the specified width and height
+    /// Returns the current zoom level
     ///
-    pub fn draw(&self, x: i32, y: i32, width: i32, height: i32) {
-        let combined = CombinedProjection::new(self.projection.as_ref(), &self.view_projection, width, height);
+    pub fn zoom(&self) -> f64 {
+        self.view_projection.zoom
+    }
+
+    ///
+    /// Sets the map zoom level
+    ///
+    pub fn set_zoom(&mut self, zoom: f64) {
+        self.view_projection.zoom = zoom
+    }
+
+    ///
+    /// Sets the geometry of this map view
+    ///
+    /// x and y are the coordinates of the lower left corner of the map view, in pixels.
+    ///
+    /// width and height are its width and height in pixels.
+    ///
+    pub fn set_geometry(&mut self, x: i32, y: i32, width: i32, height: i32) {
+        self.x = x;
+        self.y = y;
+        self.width = width;
+        self.height = height;
+    }
+
+    ///
+    /// Scrolls the map by a specified amount in pixels
+    ///
+    pub fn scroll(&mut self, dx: i32, dy: i32) {
+        let map_delta = self.view_projection.unproject(&Point { x: dx as f64, y: dy as f64 }, self.width, self.height);
+        self.view_projection.center = self.view_projection.center.clone() + map_delta;
+    }
+
+    ///
+    /// Draws this map
+    ///
+    pub fn draw(&self) {
+        let combined = CombinedProjection::new(self.projection.as_ref(), &self.view_projection, self.width, self.height);
         for layer in self.layers.iter() {
-            layer.draw(&combined, x, y, width, height);
+            layer.draw(&combined, self.x, self.y, self.width, self.height);
         }
     }
 }
